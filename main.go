65aa66c3fdecc7b5807e1235ee73a1f6c39b3da8
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-27 22:01:08 +0200

    update main and add db connection to it

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/AmjadShakhshir/Todoni/database"
 	"github.com/AmjadShakhshir/Todoni/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.DBconn()
+
     app := fiber.New()
 
     routes.Setup(app)
 
-	log.Fatal(app.Listen(":3000"))
+    log.Fatal(app.Listen(":8000"))
 }
