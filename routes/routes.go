ff3a8d0e5ff3b91afe8425c6713d5d895368ddf1
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-27 22:00:39 +0200

    add routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,18 @@
 package routes
 
 import (
+	"github.com/AmjadShakhshir/Todoni/controllers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
-    api := app.Group("/user")
+    api := app.Group("/users")
 
-    api.Get("/healthcheck", func(c *fiber.Ctx) error {
-        return c.SendString("Hello World!!")
-    })
+    // api.Get("/", controllers.User)
+
+    api.Post("/register", controllers.Register)
+
+    // api.Post("/login", controllers.Login)
+
+    // api.Post("/logout", controllers.Logout)
 }
