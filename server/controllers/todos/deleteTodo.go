dbb95d2a811e31a20ec90465ee38a74b5b927374
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-28 00:43:36 +0200

    delete todo controller2

---
+++ server/controllers/todos/deleteTodo.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/AmjadShakhshir/Todoni/database"
+	"github.com/AmjadShakhshir/Todoni/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func DeleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var todo models.Todo
+	database.DB.First(&todo, id)
+	if todo.Title == "" {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "Todo not found",
+		})
+	}
+	database.DB.Delete(&todo)
+	return c.JSON(todo)
+}
