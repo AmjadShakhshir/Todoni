7804733566801e9ad6a3fa80a5d6517f5babf729
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-27 23:45:33 +0200

    add todo models

--- server/models/models.go
+++ server/models/models.go
@@ -8,3 +8,11 @@ type User struct {
     Email    string `json:"email" gorm:"unique"`
     Password []byte `json:"-"`
 }
+
+type Todo struct {
+    gorm.Model
+    Title     string `json:"title"`
+    Content   string `json:"content"`
+    Completed bool   `json:"completed"`
+    UserID    uint   `json:"user_id"`
+}
