db52575f405aad56017024d767b6e6a1e5c46505
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-27 23:12:59 +0200

    add dbconfig function to load env variables

--- database/dbconn.go
+++ database/dbconn.go
@@ -1,25 +1,39 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/AmjadShakhshir/Todoni/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var dsn = os.Getenv("DB_URL")
-
+func dbConfig() string {
+    err := godotenv.Load(".env") // adjust the path according to your .env file location
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+	return dsn
+}
 
 var DB *gorm.DB
 
 func DBconn() {
+	dsn := dbConfig()
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 	log.Fatal(err)
 	}
-    DB = db
+		DB = db
 
     db.AutoMigrate(&models.User{})
 }
